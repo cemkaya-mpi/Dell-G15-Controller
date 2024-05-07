ed33e7c98038cc044e855d5232d13c71d770a9f5
老余 <121704835+OldFishKog@users.noreply.github.com>
2024-05-07 16:55:53 +0800

    add 5511 model

--- patch.py
+++ patch.py
@@ -3,10 +3,5 @@ def g15_5520_patch(wind):
 
 
 def g15_5511_patch(wind):
-    wind.power_modes_dict.update({"USTT_Balanced" : "0x97",
-                                  "USTT_Performance" : "0x98",
-                                  "USTT_Quiet" : "0x96",
-                                  "USTT_FullSpeed" : "0x99"
-                                      })
     del wind.power_modes_dict["USTT_FullSpeed" ]
     del wind.power_modes_dict["USTT_BatterySaver" ]
