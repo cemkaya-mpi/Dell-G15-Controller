3e4a0d5875fe65ee212b975eded6623ea5730b34
Phobeuscz <phobeus@bk.ru>
2024-05-31 09:23:29 +0200

    Updated fan_modes ACPI IDs
    "Balanced" : "0xa0"
    "Performance" : "0xa1"
    "Cool" : "0xa2"
    "Quiet" : "0xa3"
    #"USTT_FullSpeed" : "0xa4", # Hidden for 5511
    #"USTT_BatterySaver" : "0xa5", Hidden for 5511
    "G Mode" : "0xab"
    "Manual" : "0x0"

--- patch.py
+++ patch.py
@@ -3,10 +3,12 @@ def g15_5520_patch(wind):
     
 def g15_5511_patch(wind):
     wind.power_modes_dict = {
-        "Quiet" : "0x96",
-        "Balanced" : "0x97",
-        "Performance" : "0x98",
-        "FullSpeed" : "0x99",
+        "Balanced" : "0xa0",
+        "Performance" : "0xa1",
+        "Cool" : "0xa2",
+        "Quiet" : "0xa3",
+       #"USTT_FullSpeed" : "0xa4", # Hidden for 5511
+       #"USTT_BatterySaver" : "0xa5", Hidden for 5511
         "G Mode" : "0xab",
         "Manual" : "0x0",
     }
