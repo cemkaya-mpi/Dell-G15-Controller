be6a8432e3de2799bceb4c4718f607dac2e0e687
Phobeuscz <phobeus@bk.ru>
2024-06-09 13:45:52 +0200

    [G15 5511] Fix to actually make it working (#19)
    * Fix to actually make it working
    
    Tested on Dell G15 5511 1.28.0 BIOS
    G Mode works with turning off, other fan profiles too
    
    * Updated fan_modes ACPI IDs
    
    "Balanced" : "0xa0"
    "Performance" : "0xa1"
    "Cool" : "0xa2"
    "Quiet" : "0xa3"
    "G Mode" : "0xab"
    "Manual" : "0x0"
    
    * Allowing g15_5511_patch instead of None
    
    * Changed naming and simplified proposed patch.
    
    ---------
    
    Co-authored-by: Cem Kaya <cemkaya.boun@gmail.com>

--- patch.py
+++ patch.py
@@ -1,12 +1,7 @@
 def g15_5520_patch(wind):
-    del wind.power_modes_dict["USTT_FullSpeed" ]
+    del wind.power_modes_dict["USTT_FullSpeed"]
     
 def g15_5511_patch(wind):
-    wind.power_modes_dict = {
-        "Quiet" : "0x96",
-        "Balanced" : "0x97",
-        "Performance" : "0x98",
-        "FullSpeed" : "0x99",
-        "G Mode" : "0xab",
-        "Manual" : "0x0",
-    }
+    del wind.power_modes_dict["USTT_FullSpeed"]
+    del wind.power_modes_dict["USTT_BatterySaver"]
+    wind.power_modes_dict["USTT_Cool"] = "0xa2"
