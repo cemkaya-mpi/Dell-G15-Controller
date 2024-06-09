f12c677cc68823945e1b83376f821d74ec811a9a
Cem Kaya <cemkaya.boun@gmail.com>
2024-06-09 13:44:51 +0200

    Changed naming and simplified proposed patch.

--- patch.py
+++ patch.py
@@ -1,14 +1,7 @@
 def g15_5520_patch(wind):
-    del wind.power_modes_dict["USTT_FullSpeed" ]
+    del wind.power_modes_dict["USTT_FullSpeed"]
     
 def g15_5511_patch(wind):
-    wind.power_modes_dict = {
-        "Balanced" : "0xa0",
-        "Performance" : "0xa1",
-        "Cool" : "0xa2",
-        "Quiet" : "0xa3",
-       #"USTT_FullSpeed" : "0xa4", # Hidden for 5511
-       #"USTT_BatterySaver" : "0xa5", Hidden for 5511
-        "G Mode" : "0xab",
-        "Manual" : "0x0",
-    }
+    del wind.power_modes_dict["USTT_FullSpeed"]
+    del wind.power_modes_dict["USTT_BatterySaver"]
+    wind.power_modes_dict["USTT_Cool"] = "0xa2"
